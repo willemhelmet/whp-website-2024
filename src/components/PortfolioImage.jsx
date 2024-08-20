edc907f554b04d2f98ce4ea2433305fb3dc5df06
Willem Helmet <willemhelmet@gmail.com>
2024-08-19 20:02:14 -0400

    Started adding portfolio content

--- src/components/PortfolioImage.jsx
+++ src/components/PortfolioImage.jsx
@@ -1,9 +1,19 @@
-import { Plane } from "@react-three/drei";
+import { Plane, useTexture } from "@react-three/drei";
 
+/** TODO:
+ * come up with an elegant solution to
+ * keep images in their proper proprtions
+ * while still having control over its scale
+ */
 function PortfolioImage(props) {
+  const texture = useTexture(props.src);
+  const width = texture.source.data.naturalWidth * 0.001;
+  const height = texture.source.data.naturalHeight * 0.001;
   return (
     <group {...props}>
-      <Plane></Plane>
+      <Plane args={[width, height]}>
+        <meshBasicMaterial map={texture} transparent={true} />
+      </Plane>
     </group>
   );
 }
--- src/components/World.jsx
+++ src/components/World.jsx
@@ -2,6 +2,7 @@ import Floor from "./Floor.jsx";
 import { Suzanne } from "./Suzanne";
 import { Smiley } from "./Smiley";
 import { Sky } from "@react-three/drei";
+import PortfolioImage from "./PortfolioImage.jsx";
 
 function World() {
   return (
@@ -18,6 +19,26 @@ function World() {
         sunPosition={[0, 1, 0]}
         inclination={0}
         azimuth={0.25}
+      />
+      {/* Verizon 5G Stadium in Fortnite */}
+      <PortfolioImage
+        position={[-3, 0, -7.5]}
+        src={"/portfolio/5g-stadium.png"}
+      />
+      <PortfolioImage
+        position={[-4, 0, -7.5]}
+        src={"/portfolio/5g-fortnite-pic.png"}
+      />
+      {/* Hackathons */}
+      <PortfolioImage
+        position={[-5, 0, -7.5]}
+        src={"/portfolio/hack-sommilar.jpg"}
+      />
+      {/* Nike Watch Her Glow */}
+      {/* Looks like GIFs do not work */}
+      <PortfolioImage
+        position={[-6, 0, -7.5]}
+        src={"/portfolio/nike-color-bars.gif"}
       />
     </group>
   );
