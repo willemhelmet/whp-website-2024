f99c64530095d13df5502a6b055091aaca58727f
Willem Helmet <willemhelmet@gmail.com>
2024-08-20 13:49:03 -0400

    small comment to PortfolioVideo

--- src/components/PortfolioVideo.jsx
+++ src/components/PortfolioVideo.jsx
@@ -2,6 +2,8 @@ import { Plane, useVideoTexture } from "@react-three/drei";
 
 /** TODO:
  * Resize Plane so that video's dimensions are correct
+ * Hopefully, this can be done automatically
+ * Last resort I put in width and height in props
  */
 function PortfolioVideo(props) {
   const videoTexture = useVideoTexture(props.src);
